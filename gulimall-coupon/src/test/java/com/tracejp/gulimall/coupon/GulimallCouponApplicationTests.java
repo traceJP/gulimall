0633e2a2955de9c291722958a81bbef5f9d64fb4
traceJP <13717067350@163.com>
2023-02-22 21:20:27 +0800

    项目环境初始化

---
+++ gulimall-coupon/src/test/java/com/tracejp/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracejp.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/main/java/com/tracejp/gulimall/ware/GulimallWareApplication.java
@@ -0,0 +1,13 @@
+package com.tracejp.gulimall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWareApplication.class, args);
+    }
+
+}
